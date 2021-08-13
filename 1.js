1f85a43009c71f8f66498191f978209e1c7701af
f3ci <48576458+f3ci@users.noreply.github.com>
2021-08-13 16:32:57 +0700

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,60 @@
+main()
+
+function main(){
+	enumlib();
+	var pathlib = "/data/app/pathto/packagename/lib/arm64/libname.so";
+	var namelib = "yourlib.so";
+	var funclib = "yourfunc";
+	//hookretval(pathlib,namelib,funclib);
+}
+
+function enumlib(){
+	Interceptor.attach(Module.findExportByName(null, "open"), {
+		onEnter: function(args){
+			var str = args[0].readUtf8String();
+			if( str.includes(".so") ){
+				var splitted = str.split("/");
+				var libName = splitted[splitted.length-1];
+				var module = Process.findModuleByName(libName);
+				if(module != null){
+					console.log("[*] Module Found : "+JSON.stringify(module))
+					var exports = module.enumerateExports();
+					for(var j = 0; j<exports.length; j++){
+						console.log("\t"+JSON.stringify(exports[j]));
+					}
+				}
+			}
+		}
+	})
+}
+
+
+function hookretval(pathlib,namelib,funclib){
+	Interceptor.attach(Module.findExportByName(null, "open"), {
+	onEnter: function(args){
+		var str = args[0].readUtf8String();
+		if( str == pathlib ){
+			var module = Process.findModuleByName(namelib);
+			if(module != null){
+				console.log("[*] Module Found : "+JSON.stringify(module));
+				var myExportAddr = Module.findExportByName(namelib, funclib);
+				if( myExportAddr != null){
+					console.log("[*] Export addr: "+myExportAddr);
+					Interceptor.attach(myExportAddr,{
+						onEnter: function(args){
+							console.log("[*][*] args[0] value: "+JSON.stringify(args[0]));
+							//console.log("[*][*] args[0] as UTF-8: "+Memory.readCString(args[0]));
+							},
+						onLeave: function(retval){
+							console.log("\n[*] Called - onLeave [*][*] returned value: "+retval);
+							}
+						});
+					}
+				}
+			}
+		}
+	})
+}
+
+
+
